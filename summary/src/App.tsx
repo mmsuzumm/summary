702dd90c8c6e71e6e04e4b45cbcd47e7e40605f6
Matv <97804343+mmsuzumm@users.noreply.github.com>
2023-11-07 22:09:40 +0300

    feat: create header

--- summary/src/App.tsx
+++ summary/src/App.tsx
@@ -9,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<></>} />
+          <Route path="/contact" element={<></>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- summary/src/components/Layout.tsx
+++ summary/src/components/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 
+import Header from "./Header"
+
 const Layout = () => {
   return (
     <>
-      <header>Header</header>
+      <Header />
       <main>
         <Outlet />
       </main>
