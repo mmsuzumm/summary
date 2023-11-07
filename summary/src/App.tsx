0ec49b2690358992c2feaa36b984ea5354d47e13
Matv <97804343+mmsuzumm@users.noreply.github.com>
2023-11-07 20:39:10 +0300

    chore: set up a basic template

--- summary/src/App.tsx
+++ summary/src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Layout from "./components/Layout"
+import Home from "./components/Home"
 
 function App() {
-
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
