10ac368d26ceaa9504c1a2ed141f5bd56dbbbc4c
Matv <97804343+mmsuzumm@users.noreply.github.com>
2023-11-08 09:28:37 +0300

    feat: base home page struct

--- summary/src/components/Home.tsx
+++ summary/src/components/Home.tsx
@@ -1,5 +1,13 @@
+import styles from "./Home.module.scss"
+
 function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <h1 className={styles.name}>Syusyukin Matvey Alexeevich</h1>
+      <h2 className={styles.profession}>Full stack developer</h2>
+      <ul></ul>
+    </div>
+  )
 }
 
 export default Home
